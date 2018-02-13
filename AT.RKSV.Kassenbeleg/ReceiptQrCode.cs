2b08cad74544600085cc8e288062d8fc42ab0207
Christoph Wille <christoph.wille@gmail.com>
2018-02-13 08:42:44 +0100

    Add .NET Standard library for parsing QR code information Wrapped in a separate solution with .NET Core app for xplat ability of testing

---
+++ AT.RKSV.Kassenbeleg/ReceiptQrCode.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AT.RKSV.Kassenbeleg
+{
+	public class ReceiptQrCode
+	{
+		private const int ExpectedParts = 14;
+
+		private const int IdxCiperSuite = 1;
+		private const int IdxCashRegisterId = 2;
+		private const int IdxReceiptId = 3;
+		private const int IdxDate = 4;
+		private const int IdxBetragSatzNormal = 5;
+		private const int IdxBetragSatzErmaessigt1 = 6;
+		private const int IdxBetragSatzErmaessigt2 = 7;
+		private const int IdxBetragSatzNull = 8;
+		private const int IdxBetragSatzBesonders = 9;
+		private const int IdxEncryptedTurnoverCounter = 10;
+		private const int IdxCertificateSerial = 11;
+		private const int IdxPreviousReceiptHash = 12;
+		private const int IdxSignatureValue = 13;
+
+		private readonly string _qrCode;
+		private readonly bool _isValidQrCode;
+		private readonly string[] _qrCodeParts;
+
+		public ReceiptQrCode(string qrCode)
+		{
+			_qrCode = qrCode;
+
+			if (!String.IsNullOrWhiteSpace(_qrCode))
+			{
+				_qrCodeParts = _qrCode.Split('_');
+				_isValidQrCode = (_qrCodeParts.Length == ExpectedParts);
+			}
+			else
+			{
+				_isValidQrCode = false;
+			}
+		}
+
+		public bool IsValid => _isValidQrCode;
+		public string QrCode => _qrCode;
+
+		private string GetElement(int idx)
+		{
+			if (!_isValidQrCode) return null;
+			return _qrCodeParts[idx];
+		}
+
+		public string CipherSuite => GetElement(IdxCiperSuite);
+		public string CashRegisterId => GetElement(IdxCashRegisterId);
+		public string ReceiptId => GetElement(IdxReceiptId);
+		public string Date => GetElement(IdxDate);
+		public string CertificateSerial => GetElement(IdxCertificateSerial);
+		public string SignatureValue => GetElement(IdxSignatureValue);
+	}
+}
---
+++ test-parseqrcode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using AT.RKSV.Kassenbeleg;
+
+namespace test_parseqrcode
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			string qrCode1 =
+				"_R1-AT1_fiskaltrust1_ft1C905#105218_2018-02-08T12:37:34_0,00_16,30_0,00_0,00_0,00_xfWUwBw=_7b164a88_6w3fQw5bEog=_irdxIo1TAowB1OzpU+dgeAS887k8AuT09jrcMjZx95xHzbKp5pLQcupkbpZK5UxtDaxj08+8bRO30Y4wxiwonw==";
+
+			var test = new ReceiptQrCode(qrCode1);
+			string certSerial = test.CertificateSerial;
+
+			// TODO: lookup cert by serial to be able to verify ECDSA sig
+		}
+	}
+}
